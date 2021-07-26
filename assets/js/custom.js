5d043c7205e57f882ef3986efc98f08c7c1822d3
Guilherme Cota <guilherme.cota@gerencianet.com.br>
2021-07-26 12:45:01 -0300

    atualização de elementos

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,17 +1,23 @@
-var members = document.getElementById('members');
-var suporte_team = document.getElementById('suporte-team');
+var membros = document.getElementById('membros');
+var suporte_team = document.getElementById('time-suporte');
 var duration = 2500; // 5 segundos
 
-// Community member counter
+// Contador de membros da comunidade
 for (var i = 1; i <= 1500; i++) {
     setTimeout(function (nr) {
-        members.innerHTML =  "+" + nr;
+        membros.innerHTML = "+" + nr;
     }, i * duration / 1500, i);
 }
 
-// Technical support member counter
+// Contador de membros da Consultoria Técnica
 for (var i = 1; i <= 10; i++) {
     setTimeout(function (nr) {
-        suporte_team.innerHTML =  "+" + nr;
+        suporte_team.innerHTML = "+" + nr;
     }, i * duration / 10, i);
 }
+
+/**
+ * Retorna o ano atual
+ */
+new Date().getFullYear();
+document.getElementById("year").innerHTML = new Date().getFullYear();
